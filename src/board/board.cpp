2821aeee0cdd0905494993f7d36cd2771b6db12d
jaenek <jaenek@protonmail.com>
2020-10-25 23:43:21 +0100

    Fix compiler warnings

--- src/board/board.cpp
+++ src/board/board.cpp
@@ -1,11 +1,11 @@
 #include "board.hpp"
 
-Board::Board(unsigned int rows, unsigned int cols, unsigned int cell_w, unsigned int cell_h, float x, float y)
-    : rows(rows), cols(cols), board(x, y, rows * cell_w, cols * cell_h)
+Board::Board(unsigned int nrows, unsigned int ncols, unsigned int cell_w, unsigned int cell_h, float x, float y)
+    : rows(nrows), cols(ncols), board(x, y, static_cast<float>(rows * cell_w), static_cast<float>(cols * cell_h))
 {
-	for (int i = 0; i < rows * cols; i++) {
-		unsigned int cell_x = to_row(i) * cell_w + board.x;
-		unsigned int cell_y = to_col(i) * cell_h + board.y;
+	for (unsigned int i = 0; i < rows * cols; i++) {
+		unsigned int cell_x = to_row(i) * cell_w + static_cast<unsigned int>(board.x);
+		unsigned int cell_y = to_col(i) * cell_h + static_cast<unsigned int>(board.y);
 		cells.emplace_back(cell_x, cell_y, cell_w, cell_h);
 	}
 }
@@ -13,7 +13,7 @@ Board::Board(unsigned int rows, unsigned int cols, unsigned int cell_w, unsigned
 void Board::update_highlight(const raylib::Vector2 &mouse_pos)
 {
 	if (board.CheckCollision(mouse_pos))
-		highlighted_index = to_index(mouse_pos);
+		highlighted_index = static_cast<int>(to_index(mouse_pos));
 	else
 		highlighted_index = -1;
 }
@@ -34,24 +34,28 @@ void Board::update_selected(const raylib::Vector2 &mouse_pos)
 
 void Board::draw()
 {
-	for (int i = 0; i < cells.size(); i++) {
-		if (i == highlighted_index)
+	for (unsigned int i = 0; i < cells.size(); i++) {
+		if (i == static_cast<unsigned int>(highlighted_index))
 			cells[i].DrawLinesEx(2, highlighted_color);
 		else
 			cells[i].DrawLinesEx(2, normal_color);
 	}
 
 	for (auto &i : selected_cells) {
-		selected_color.DrawLine(cells[i].x + 2, cells[i].y + 2, cells[i].x + cells[i].width - 2,
-					 cells[i].y + cells[i].height - 2);
-		selected_color.DrawLine(cells[i].x + 2, cells[i].y + cells[i].height - 2,
-					 cells[i].x + cells[i].width - 2, cells[i].y + 2);
+		raylib::Vector2 top_left = { cells[i].x + 2, cells[i].y + 2 };
+		raylib::Vector2 bottom_right = { cells[i].x + cells[i].width - 2, cells[i].y + cells[i].height - 2 };
+		selected_color.DrawLine(top_left, bottom_right);
+
+		raylib::Vector2 top_right = { cells[i].x + 2, cells[i].y + cells[i].height - 2 };
+		raylib::Vector2 bottom_left = { cells[i].x + cells[i].width - 2, cells[i].y + 2 };
+		selected_color.DrawLine(top_right, bottom_left);
+
 	}
 }
 
 unsigned int Board::to_index(const raylib::Vector2 &v) const
 {
-	unsigned int row = (v.y - board.y) / board.height * rows;
-	unsigned int col = (v.x - board.x) / board.width * cols;
-	return col + row * cols;
+	float row = (v.y - board.y) / board.height * static_cast<float>(rows);
+	float col = (v.x - board.x) / board.width * static_cast<float>(cols);
+	return static_cast<unsigned int>(col) + static_cast<unsigned int>(row) * cols;
 }
--- src/board/board.hpp
+++ src/board/board.hpp
@@ -10,7 +10,7 @@
 class Board
 {
 public:
-	Board(unsigned int rows, unsigned int cols, unsigned int cell_w, unsigned int cell_h, float x = 0, float y = 0);
+	Board(unsigned int nrows, unsigned int ncols, unsigned int cell_w, unsigned int cell_h, float x = 0, float y = 0);
 	void update_highlight(const raylib::Vector2 &mouse_pos);
 	void update_selected(const raylib::Vector2 &mouse_pos);
 	inline const std::vector<unsigned int> &get_data() const { return selected_cells; };
@@ -21,14 +21,14 @@ private:
 	raylib::Rectangle board;
 	std::vector<raylib::Rectangle> cells;
 	std::vector<unsigned int> selected_cells;
-	unsigned int highlighted_index = -1;
+	int highlighted_index = -1;
 	raylib::Color normal_color = raylib::Color::RayWhite;
 	raylib::Color highlighted_color = raylib::Color::Blue;
 	raylib::Color selected_color = raylib::Color::Red;
 
-	inline int to_row(int i) const { return i % cols; }
+	inline unsigned int to_row(unsigned int i) const { return i % cols; }
 
-	inline int to_col(int i) const { return i / cols; }
+	inline unsigned int to_col(unsigned int i) const { return i / cols; }
 
 	unsigned int to_index(const raylib::Vector2 &v) const;
 };
