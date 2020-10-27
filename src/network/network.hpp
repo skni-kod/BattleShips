71c190d4cce08f7964c0ebe02ae72179a6281044
jaenek <jaenek@protonmail.com>
2020-10-27 23:54:51 +0100

    Add message class

--- src/network/network.hpp
+++ src/network/network.hpp
@@ -2,25 +2,57 @@
 
 #define ASIO_STANDALONE
 
-#include <iostream>
 #include <asio.hpp>
 #include <asio/ts/buffer.hpp>
 #include <asio/ts/internet.hpp>
+#include <iostream>
 
-namespace network {
-	void test() {
-		asio::error_code e;
-		asio::io_context context;
+namespace network
+{
 
-		asio::ip::tcp::endpoint endpoint(asio::ip::make_address("93.184.216.34"), 80);
+enum class MessageType {
+	Connection,
+	Shots,
+};
 
-		asio::ip::tcp::socket socket(context);
+struct MessageHeader {
+	MessageType type;
+	size_t size;
+};
 
-		socket.connect(endpoint, e);
+class Message
+{
+	MessageHeader header;
+	std::vector<uint8_t> body;
 
-		if(!e)
-			std::cout << "Connected!\n";
-		else
-			std::cout << "Failed to connect to address:\n" << e.message();
+	size_t size() const { return sizeof(header) + body.size(); }
+
+public:
+	template <typename T> void add(const T &data)
+	{
+		static_assert(std::is_standard_layout<T>::value, "Data cannot be copied!");
+
+		size_t last_size = body.size();
+
+		body.resize(body.size() + sizeof(T));
+
+		std::memcpy(body.data() + last_size, &data, sizeof(T));
+
+		header.size = size();
 	}
-}
+
+	template <typename T> void pop(T &data)
+	{
+		static_assert(std::is_standard_layout<T>::value, "Data cannot be copied!");
+
+		size_t size_after = body.size() - sizeof(T);
+
+		std::memcpy(&data, body.data() + size_after, sizeof(T));
+
+		body.resize(size_after);
+
+		header.size = size();
+	}
+};
+
+} // namespace network
